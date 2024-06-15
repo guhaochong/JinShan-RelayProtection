92987cf9441c3b6860496c00d6c17f4f5ea603b8
guhaochong <guhaochong@outlook.com>
2024-06-15 11:54:43 +0800

    更新 config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  base: '/JinShan-RelayProtection/',
   title: '你的站点名称',
   description: '站点描述',
   themeConfig: {
@@ -14,16 +15,6 @@ module.exports = {
       }
     ],
     sidebar: {
-      '/': [
-        {
-          text: '指南',
-          items: [
-            { text: '开始', link: '/guide/01' },
-            { text: '安装', link: '/guide/02' },
-            { text: '配置', link: '/guide/03' }
-          ]
-        }
-      ],
       '/guide/': [
         {
           text: '指南',
@@ -34,6 +25,27 @@ module.exports = {
           ]
         }
       ]
+    },
+    locales: {
+      '/': {
+        lang: 'zh-CN',
+        title: '你的站点名称',
+        description: '站点描述',
+        label: '简体中文',
+        selectText: '选择语言',
+        ariaLabel: '选择语言',
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdated: '上次更新',
+        outlineTitle: '本页内容', 
+        next: '下一页',
+        prev: '上一页',
+        serviceWorker: {
+          updatePopup: {
+            message: "发现新内容可用。",
+            buttonText: "刷新"
+          }
+        }
+      }
     }
   }
 }
