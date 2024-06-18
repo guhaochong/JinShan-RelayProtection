2064c48b49ca4e6234c74af813b2927109f33891
guhaochong <guhaochong@outlook.com>
2024-06-18 15:01:32 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,7 @@ export default defineUserConfig({
   title: 'JinShan-RelayProtection',
   description: 'JinShan-RelayProtection Site',
 
+  base: '/JinShan-RelayProtection/',
 
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
