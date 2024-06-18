85b6d352a7e907735680f07f2ba4842084eec9f4
guhaochong <guhaochong@outlook.com>
2024-06-18 15:46:25 +0800

    更新

---
+++ docs/.vuepress/.temp/pages/Legacydata/.~10K.html.js
@@ -0,0 +1,16 @@
+import comp from "D:/Web/Github/JinShan-RelayProtection/docs/.vuepress/.temp/pages/Legacydata/.~10K.html.vue"
+const data = JSON.parse("{\"path\":\"/Legacydata/.~10K.html\",\"title\":\"10千伏 K型 标准 二次铭牌表\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"Legacydata/.~10K.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
