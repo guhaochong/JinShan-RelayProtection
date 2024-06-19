112a63d1a5b81813277e16a16dea3e7b7512d51e
guhaochong <guhaochong@outlook.com>
2024-06-19 08:42:36 +0800

    更新

---
+++ docs/.vuepress/.temp/pages/about.html.js
@@ -0,0 +1,3 @@
+import comp from "D:/Web/Github/JinShan-RelayProtection/docs/.vuepress/.temp/pages/about.html.vue"
+const data = JSON.parse("{\"path\":\"/about.html\",\"title\":\"Home\",\"lang\":\"zh-CN\",\"frontmatter\":{\"home\":true,\"title\":\"Home\",\"heroImage\":\"/images/goodlogo.png\",\"actions\":[{\"text\":\"开始\",\"link\":\"/get-started.html\",\"type\":\"primary\"},{\"text\":\"关于\",\"link\":\"/#\",\"type\":\"secondary\"}],\"footer\":\"MIT Licensed | Copyright © 2024 金山继保\"},\"headers\":[],\"git\":{},\"filePathRelative\":\"about.md\"}")
+export { comp, data }
--- docs/.vuepress/.temp/pages/get-started.html.vue
+++ docs/.vuepress/.temp/pages/get-started.html.vue
@@ -2,11 +2,16 @@
 <p>这是一个快速索引页面，在这里可以按分类找到你需要的数据表</p>
 <h2 id="点表库" tabindex="-1"><a class="header-anchor" href="#点表库"><span>点表库</span></a></h2>
 <ul>
-<li><RouteLink to="/Legacydata/10K.html">10千伏 K型号 标准</RouteLink></li>
-<li>002</li>
-<li>003</li>
+<li><RouteLink to="/Page/10K-Standard-Uploadplate.html">10千伏 K型号 标准 点表</RouteLink></li>
+<li><RouteLink to="/Page/10K-SF6-Uploadplate.html">10千伏 K型号 SF6充气型 点表</RouteLink></li>
+<li><RouteLink to="/Page/10P-Standard-Uploadplate.html">10千伏 P型号 标准 点表</RouteLink></li>
+<li><RouteLink to="/Page/10K-Automatic-Uploadplate.html">10千伏 K型号 自愈 点表</RouteLink></li>
 </ul>
 <h2 id="铭牌库" tabindex="-1"><a class="header-anchor" href="#铭牌库"><span>铭牌库</span></a></h2>
+<ul>
+<li><RouteLink to="/Page/10K-Standard-Nameplate.html">10千伏 K型号 标准 二次铭牌</RouteLink></li>
+<li></li>
+</ul>
 </div></template>
 
 
